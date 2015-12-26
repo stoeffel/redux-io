54fef192e79285f73f8090cb7ef566d1dbea5136
Stoeffel <schtoeffel@gmail.com>
2015-12-26 14:08:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+redux-io
+============
+
+[![build status](https://img.shields.io/travis/stoeffel/redux-io/master.svg?style=flat-square)](https://travis-ci.org/stoeffel/redux-io)
+[![npm version](https://img.shields.io/npm/v/redux-io.svg?style=flat-square)](https://www.npmjs.com/package/redux-io)
+
+[FSA](https://github.com/acdlite/flux-standard-action)-compliant io monad [middleware](https://github.com/gaearon/redux/blob/master/docs/middleware.md) for Redux.
+
+This is based on [redux-future](https://github.com/stoeffel/redux-future).
+
+
+```js
+npm install --save redux-io
+```
+
+## Usage
+
+```js
+import ioMiddleware from 'redux-io';
+const createStoreWithMiddleware = applyMiddleware(
+  ioMiddleware('runIO') // <- function name of the function to run the IO.
+)(createStore)
+```
+
+
+### Example
+
+```js
+const io = IO(() => {
+  document.title = "Goodbye World!";
+  return { type: 'INCREMENT' };
+});
+store.dispatch(io);
+
+```
+
+## Using in combination with redux-actions
+
+Because it supports FSA actions, you can use redux-io in combination with [redux-actions](https://github.com/acdlite/redux-actions).
+
+### Example: Action creators
+
+```js
+const getHrefIO = IO(() => location.href);
+store.dispatch({ type: 'ACTION', payload: getHrefIO });
+```
+
+## What's an IO?
+* [mostly-adequate-guide  Chapter 8.5 Old McDonald had Effects...](https://drboolean.gitbooks.io/mostly-adequate-guide/content/ch8.html)
---
+++ test/test.js
@@ -0,0 +1,75 @@
+import expect from 'expect';
+import { createStore, applyMiddleware } from 'redux';
+import { createAction } from 'redux-actions';
+import R from 'ramda';
+import { IO } from 'ramda-fantasy';
+
+import ioMiddleware from '../src';
+
+
+describe('redux-io', () => {
+  let store, unsubscribe;
+
+  before(() => {
+    const initialState =
+      { counter: 0
+      , fsa: 'failed'
+      };
+
+    function counter(state = initialState, action) {
+      switch (action.type) {
+      case 'INCREMENT':
+        return { ... state
+               , counter: state.counter + 1
+               };
+      case 'FSA_ACTION':
+        return { ... state
+               , fsa: action.payload
+               };
+      default:
+        return state
+      }
+    }
+
+    const createStoreWithMiddleware = applyMiddleware(
+      ioMiddleware('runIO')
+    )(createStore)
+
+    store = createStoreWithMiddleware(counter);
+  });
+
+
+  afterEach(() => {
+    unsubscribe();
+  });
+
+  it('should work with an io', done => {
+    const spy = expect.createSpy()
+
+    unsubscribe = store.subscribe(() => {
+      expect(store.getState().counter).toEqual(1);
+      expect(spy).toHaveBeenCalled()
+      done();
+    });
+    const io = IO(() => {
+      spy();
+      return { type: 'INCREMENT' };
+    });
+    store.dispatch(io);
+  });
+
+  it('should work with FSA', done => {
+    const spy = expect.createSpy()
+
+    unsubscribe = store.subscribe(() => {
+      expect(store.getState().fsa).toEqual('test');
+      expect(spy).toHaveBeenCalled()
+      done();
+    });
+    const io = IO(() => {
+      spy();
+      return 'test';
+    });
+    store.dispatch({ type: 'FSA_ACTION', payload: io });
+  });
+});
