5eafff9d0f22eda43e42b85e484a8e2fa4802289
Stoeffel <schtoeffel@gmail.com>
2015-12-26 14:42:18 +0100

    add missing files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { isFSA } from 'flux-standard-action';
+
+
+const typeOf = (type) => (val) => typeof val === type;
+const isFunction = typeOf('function');
+
+export default runIO => function futureMiddleware({ dispatch }) {
+  const isIO = io => io && isFunction(io[runIO]);
+
+  return next => action => {
+    if (!isFSA(action)) {
+      return isIO(action)
+        ? dispatch(action[runIO]())
+        : next(action);
+    }
+
+    return isIO(action.payload)
+      ? dispatch({ ...action, payload: action.payload[runIO]() })
+      : next(action);
+  };
+}
